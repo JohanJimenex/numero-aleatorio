6374b236463c522869815e2caf75b926536df8ac
johan <johan.jimenez.23@gmail.com>
2019-11-04 01:08:55 -0400

    Agregado notas para mejor comprension para los usuarios

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
-var numeros = document.querySelector("#input");
-var boton = document.querySelector("#boton");
+var numeroIngresado = document.querySelector("#input");
+var botonSortear = document.querySelector("#boton");
 var contenedor = document.querySelector("#contenedor");
-var carga = document.querySelector("#carga");
 
-boton.addEventListener("click", sortear);
+botonSortear.addEventListener("click", sortear);
 
 function sortear(e) {
     e.preventDefault();
 
-    contenedor.innerHTML = ' <div id="carga" class="mb-4 spinner-border text-warning display-1 " role="status"></div> ';
+    //Muestra animacion del circulo cargando, las clases para la animacion de css son agregadas con bootstrap.css
+    contenedor.innerHTML = '<div id="carga" class="mb-4 spinner-border text-warning display-1 " role="status"></div>';
 
-
+    //Esta funtion crea un intervalo de tiempo, luego pasado 2 segundos ejecuta una accion
     setTimeout(() => {
 
-        var numeroGanador = Math.ceil(Number(numeros.value) * Math.random());
+        //Math.ceil() redondea un numero hacia arriba y Math.random() genera un numero aleatorio desde el 0 hasta el 1 ej: 0.54
+        var numeroGanador = Math.ceil(Number(numeroIngresado.value) * Math.random());
 
+        //Reemplaza el contenido (actualmente la animacion) por el numero generado aleatoriamente
         contenedor.innerHTML = numeroGanador;
 
     }, 2000);
